0cf057881a5c78cfe86674a35ba43c7ac8ed5fa8
maryamtb <maryamtb.17@gmail.com>
2020-04-04 21:06:23 +0100

    anim update

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
-// console.log('hi')
 const header = document.querySelector("#header");
 
 const banner = document.querySelector("#bannerBg");
-const bannerFirst = document.querySelector("#bannerBgFirst");
 const bannerAddition = document.querySelector("#bannerBgAddition");
 
 const h1Line = document.querySelectorAll(".line span");
+
 const contentP = document.querySelector(".content-inner p");
 const contentButton = document.querySelector(".content-inner .btn-row");
+
+const box = document.querySelector("#box");
+
+const grain = document.querySelector(".image-inner img");
 
 const bannerTL = gsap.timeline();
 
 bannerTL
-  .from([bannerAddition, bannerFirst, banner], {
+  .from([bannerAddition, banner], {
     duration: 1.2,
     width: 0,
-    skewX: 4,
+    skewX: -4,
     ease: "power3.inOut",
     stagger: {
       amount: 0.2,
@@ -47,3 +50,21 @@ bannerTL
       amount: 0.2,
     },
   });
+
+gsap.from(box, {
+  delay: 2.2,
+  x: -100,
+  skewX: 2,
+  duration: 0.8,
+  opacity: 0,
+  ease: "power3.out",
+});
+
+gsap.from(grain, {
+  delay: 2.2,
+  x: -100,
+  skewX: 4,
+  duration: 0.8,
+  opacity: 0,
+  ease: "power3.out",
+});
