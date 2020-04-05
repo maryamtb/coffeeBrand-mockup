4a9d92a9a07fc0635516556a966dc39433147829
maryamtb <maryamtb.17@gmail.com>
2020-04-05 11:02:37 +0100

    design updates

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const h1Line = document.querySelector(".line span");
 const contentP = document.querySelector(".content-inner p");
 const contentButton = document.querySelector(".content-inner .btn-row");
 
-const box = document.querySelector(".image #box");
+// const box = document.querySelector(".image #box");
 
 const grain = document.querySelector(".image-inner img");
 
@@ -18,7 +18,7 @@ bannerTL
   .from([bannerAddition, banner], {
     duration: 1.2,
     width: 0,
-    skewX: -4,
+    skewX: 0.2,
     ease: "power3.inOut",
     stagger: {
       amount: 0.2,
@@ -55,14 +55,14 @@ bannerTL
 //   opacity: 0,
 // })
 
-gsap.from(box, {
-  delay: 2.2,
-  x: -1,
-  skewX: 1,
-  duration: 0.8,
-  opacity: 0,
-  ease: "power3.out",
-});
+// gsap.from(box, {
+//   delay: 2.2,
+//   x: -1,
+//   skewX: 1,
+//   duration: 0.8,
+//   opacity: 0,
+//   ease: "power3.out",
+// });
 
 gsap.from(grain, {
   delay: 2.2,
